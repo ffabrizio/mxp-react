ee324dd89f5e5cad946c61c232a0a23da137d028
Fabio Fabrizio <ffabrizio@gmail.com>
2016-05-31 23:55:38 +0100

    optimise nav

--- app/components/topNav.js
+++ app/components/topNav.js
@@ -5,23 +5,22 @@ class TopNav extends Component {
   constructor(props) {
     super(props)
     
-    console.log(props)
-    
     this.state = {
       style: {
         position: 'fixed',
         right: 0,
         top: 0,
         width: '100%',
-        height: 38,
         zIndex: 20
       },
-      closed: true,
-      selected: props.links[0].name
+      submenuclosed: true,
+      shortcutsclosed: true,
+      menuselected: props.links[0].key.toLowerCase() || '',
+      shortcuts: [props.links[0].links[2], props.links[1].links[0]]
     }
+    
   }
-  
-  
+
   
   render() {
     let wrapperStyle = {
@@ -37,53 +36,92 @@ class TopNav extends Component {
       flexDirection: 'column',
       flexGrow: 0,
       flexShrink: 0,
-      padding: 10
+      padding: 7
     }
 
     let onClick = (e) => {
+      let a = e.target.getElementsByTagName('a')
+      let key = (a.innerText || e.target.innerText).toLowerCase()
+      let changed = this.state.menuselected != key
+      
       this.setState( { 
-        selected: e.target.innerText 
+        menuselected: (a.innerText || e.target.innerText).toLowerCase()
       })
       
-      if (this.state.closed) {
-        this.setState( { 
-          closed: false 
-        })
+      this.setState({shortcutsclosed: true})
+      if (this.state.submenuclosed) {
+        this.setState( {submenuclosed: false})
+      } else if (!changed) {
+        this.setState( {submenuclosed: true})
+      }
+    }
+    
+    let onContainerClick = (e) => {
+      if (!e.target.href) {
+        this.setState({submenuclosed: true})
+        this.setState({shortcutsclosed: true})
+      }
+    }
+    
+    let onShortcutsClick = () => {
+      this.setState({submenuclosed: true})
+      if (this.state.shortcutsclosed) {
+        this.setState( { shortcutsclosed: false })
       } else {
-        this.setState( { 
-          closed: true 
-        })
+        this.setState( {shortcutsclosed: true})
       }
     }
     
     const createItems = () => {
       return this.props.links.map((item) => {
-        return <span key={item.name} style={itemStyle}>{item.name}</span>
+        return <a className="nav" onClick={onClick} href={item.url} key={item.key} style={itemStyle}>{item.name}</a>
       })
     }
 
     const createSubItems = () => {
       return this.props.links.map((item) => {
-        if (item.name === this.state.selected) {
+        if (item.key.toLowerCase() === this.state.menuselected) {
            return item.links.map((item) => {
-             return <a className="nav" href={item.url} key={item.name} style={itemStyle}><img className="img" src="http://placehold.it/350x150" /><br />{item.name}</a>
+             return <a className="nav" href={item.url} key={item.name} style={itemStyle}><img className="img" src="http://placehold.it/250x100" /><h5>{item.name}</h5></a>
            })
         }
       })
     }
     
+    const createShortcuts = () => {
+      return this.state.shortcuts.map((item) => {
+        return <a className="nav" href={item.url} key={item.name} style={itemStyle}><img className="img" src="http://placehold.it/250x100" /><h5>{item.name}</h5></a>
+      })
+    }
+    
+    const visibleToggle = (isHidden) => {
+      return { display: (isHidden ? 'none' : 'block') }
+    }
+    
     return (
         <div style={this.state.style}>
-          <div className="mainNav" onClick={onClick}>
+          <div className="main-nav" onClick={onContainerClick}>
           
             <div style={wrapperStyle}>
               { createItems() }
+              <a className="nav last" onClick={onShortcutsClick} href="#" key="quick menu" style={itemStyle}>
+                Shortcuts 
+              </a>
             </div>
             
           </div>
-          <div className="promoNav" style={{ display: (this.state.closed ? 'none' : 'block') }}>
+          <div className="promo-nav" 
+            style={visibleToggle(this.state.submenuclosed)} 
+            onClick={onContainerClick}>
             <div style={wrapperStyle}>
               { createSubItems() }
+            </div>
+          </div>
+          <div className="promo-nav" 
+            style={visibleToggle(this.state.shortcutsclosed)}
+             onClick={onContainerClick}>
+            <div style={wrapperStyle}>
+              { createShortcuts() }
             </div>
           </div>
         </div>
