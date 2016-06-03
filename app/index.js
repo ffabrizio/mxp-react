0ab468d39d07dc1f2952eaffb43a69e930170d4d
Fabio Fabrizio <f.fabrizio@syzygy.co.uk>
2016-06-03 13:31:53 +0100

    Backend integration test

--- app/index.js
+++ app/index.js
@@ -7,31 +7,18 @@ import Text from './components/text'
 import TopNav from './components/topNav'
 import TestAnimation from './components/testanimation'
 
-const moduleMap = {
-  'container' : Container,
-  'video' : Video,
-  'promo' : Promo,
-  'text' : Text,
-  'nav' : TopNav
+window.pageData.moduleMap['package1.title'] = TestAnimation
+window.pageData.moduleMap['package1.container'] = Container
+window.pageData.moduleMap['package1.video'] = Video
+window.pageData.moduleMap['package1.promo'] = Promo
+window.pageData.moduleMap['package1.text'] = Text
+window.pageData.moduleMap['package1.nav'] = TopNav
+
+var moduleMapper = function(module) {
+  if (module.modules) module.props.children = module.modules.map(moduleMapper);
+  return React.createElement(window.pageData.moduleMap[module.type], module.props);
 }
 
-const moduleMapper = (module) => {
-  if (module.modules) module.props.children = module.modules.map(moduleMapper)
-  return (
-    React.createElement(moduleMap[module.type], module.props)
-  )
-}
-
-const modules = window.pageData.modules.map(moduleMapper)
-const root = document.getElementById('root')
-const title = document.getElementsByTagName('h1', root)[0].innerText
-
-ReactDOM.render(
-  <div>
-    <TestAnimation copy={title} />
-    <Container>
-      { modules }
-    </Container>
-  </div>
-  , document.getElementById('root')
-)
+var modules = window.pageData.modules.map(moduleMapper);
+var reactRoot = React.createElement('div', {}, modules);
+ReactDOM.render(reactRoot, document.getElementById('root'))
